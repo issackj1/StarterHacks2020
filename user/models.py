4f6fb22ac23928e22e5ee54a8832a58a8fb52204
billwu99 <billwu95@gmail.com>
2020-01-18 17:56:45 -0500

    model for user

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    current_weight = models.DecimalField(max_digits=10, decimal_places=2)
+    desired_weight = models.DecimalField(max_digits=10, decimal_places=2)
+    increment = models.DecimalField(max_digits=10, decimal_places=2)
+    email = models.EmailField(max_length=254)
+    date = models.DateTimeField(default=datetime.now())
+    def __str__(self):
+        return "{} {} {}".format(self.first_name, self.last_name, self.email)
+        
--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import JsonResponse, HttpResponse
-import requests
 
 class HomeView(TemplateView):
     template_name = "user/home.html"
+
