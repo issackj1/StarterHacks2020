3a24d4c58cb49dc53c24e5acdae6ff9bb496c059
issackj1 <issack.john1@ucalgary.ca>
2020-01-19 00:32:26 -0500

    return search food url

--- user/urls.py
+++ user/urls.py
@@ -11,8 +11,9 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('about/', views.AboutView.as_view(), name='about'),
     path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
-    #path('personaldata/', views.PersonalData.as_view(), name='personaldata'),
+    # path('personaldata/', views.PersonalData.as_view(), name='personaldata'),
     url(r'^data$', views.data, name='data'),
+    path('search-food/', views.SearchView.as_view(), name='search-food'),
     path('search/<str:input>/', views.search_box, name='search'),
     path('details/<str:user_choices>/', views.FoodView, name='data')
 ]
